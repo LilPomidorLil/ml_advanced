1579eb4605807c372d842ea537dd9a68cfceaf30
LilPomidorLil <rozhinthe@ya.ru>
2022-05-17 23:57:07 +0300

    небольшие изменения в эксп. 3.3

--- ml_optimization/practice_1/Experiment/3.3.py
+++ ml_optimization/practice_1/Experiment/3.3.py
@@ -26,13 +26,11 @@ def accuracy_log(y_true, y_pred):
     all = len(y_true)
     return sum_equal / all
 
-def plot_grad_nwtn_graph(grad_acc: List[float], nwtn_acc: List[float], eps_list: List[float], filename: str):
-    plt.title("Gradient Descent / Newton")
-    plt.plot(eps_list, grad_acc, color = 'black', label = "Gradient Descent")
-    plt.plot(eps_list, nwtn_acc, color = 'red', label = "Newton Method")
+def plot_nwtn_graph(nwtn_acc: List[float], eps_list: List[float], filename: str):
+    plt.title("Newton Method")
+    plt.plot(eps_list, nwtn_acc, color = 'red', linewidth = 6)
     plt.xlabel("Epsilon")
     plt.ylabel("Accuracy")
-    plt.legend()
     plt.grid()
     plt.savefig('../Report/img/3.3/{}.eps'.format(filename.replace('.txt', '')), format='eps')
 
@@ -42,18 +40,11 @@ def plot(filename: str, method_name: str, eps_list: List[float]):
     except:
         return
 
-    acc_grad_list = []
     acc_nwtn_list = []
     oracle = oracles.create_log_reg_oracle(X, y, 1 / len(y), oracle_type='usual')
     x_0 = np.zeros(X.toarray().shape[1])
 
     for eps in eps_list:
-        x_star_grad, _, history_grad = optimization.gradient_descent(oracle,
-                                                                x_0,
-                                                                tolerance=eps,
-                                                                line_search_options={"method": method_name},
-                                                                trace = True,
-                                                                display = False)
         x_star_nwtn, _, history_nwtn = optimization.newton(oracle,
                                                            x_0,
                                                            tolerance=eps,
@@ -61,18 +52,13 @@ def plot(filename: str, method_name: str, eps_list: List[float]):
                                                            trace = True,
                                                            display = False)
 
-        y_pred_grad = np.sign(X @ x_star_grad)
         y_pred_nwtn = np.sign(X @ x_star_nwtn)
-
-        acc_grad_list.append(
-            accuracy_log(y, y_pred_grad)
-        )
 
         acc_nwtn_list.append(
             accuracy_log(y, y_pred_nwtn)
         )
 
-    plot_grad_nwtn_graph(acc_grad_list, acc_nwtn_list, eps_list, filename)
+    plot_nwtn_graph(acc_nwtn_list, eps_list, filename)
 
 
     print(SEPARETED_TEXT, SUCCESS_WORD, SEPARETED_TEXT, sep = '\n', end = '\n\n\n')
